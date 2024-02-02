2c8e21991dd906081237603c33bc8fc7e4083c91
Chetan Joshi <chetan.j@antino.io>
2024-02-02 17:03:14 +0530

    chainging the porduction url

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,14 @@ class _MyWebViewState extends State<MyWebView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Onmeta Example'),
+        title: Text('Onmeta Production'),
       ),
       body: WebView(
         initialUrl:
-            'https://p2xvk012-3000.inc1.devtunnels.ms/?apiKey=6af803df-0e0e-4708-805f-87baa8653e97', // Set the initial URL here
+            'https://platform.onmeta.in/?apiKey=6af803df-0e0e-4708-805f-87baa8653e97', // Set the initial URL here
         javascriptMode: JavascriptMode.unrestricted, // Enable JavaScript
       ),
     );
   }
 }
+            // 'https://p2xvk012-3000.inc1.devtunnels.ms/?apiKey=6af803df-0e0e-4708-805f-87baa8653e97', // Set the initial URL here
